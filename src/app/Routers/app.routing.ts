9cb19106df716ba1406296f211c4e32fe852e490
Francisco blanco <franjav9010@hotmail.com>
2020-10-14 18:16:45 -0500

    Se empeiza con la mquetacion de la pagina san felipe

--- src/app/Routers/app.routing.ts
+++ src/app/Routers/app.routing.ts
@@ -1,10 +1,11 @@
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { AppComponent } from '../app.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from '../Components/layout/layout.component';
 
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: 'Home', pathMatch: 'full' },
+    { path: '', component: LayoutComponent },
     { path: '*', component: AppComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { routing } from './Routers/app.routing';
+import { LayoutComponent } from './Components/layout/layout.component';
+import { LoginComponent } from './Components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
