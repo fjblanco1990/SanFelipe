08888d2c70865af6e48e6a19dc8014d8f41e1415
Francisco blanco <franjav9010@hotmail.com>
2020-11-02 13:51:47 -0500

    Se realizan avances y cambios

--- src/app/Components/layout/layout.component.ts
+++ src/app/Components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-
+  @ViewChild('inputMessage')
+  inputMessageRef: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+ 
 }
